e79c5f2421f3b694f90ba359a6f8d272bec5bd4c
Samuel Gago <a89837@ualg.pt>
2025-11-19 20:41:55 +0000

    Criei o menu de clientes

---
+++ src/cliente.py
@@ -0,0 +1,11 @@
+def menu_cliente():
+    while True:
+        print("\n--- Menu Cliente ---")
+        print("1 - Ver catálogo")
+        print("2 - Adicionar produto ao carrinho")
+        print("3 - Ver carrinho")
+        print("4 - Remover do carrinho")
+        print("5 - Filtrar produtos por preço")
+        print("6 - Finalizar compra")
+        print("0 - Voltar")
+        opcao = input("Escolha: ")
